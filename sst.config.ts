35124d90949a969b986b9aa530ec27cc6d947ad3
Phúc Nguyễn <40336551+phucnguyen035@users.noreply.github.com>
2023-10-10 01:30:27 +0700

    feat: configure sst and github actions

---
+++ sst.config.ts
@@ -0,0 +1,32 @@
+import type { SSTConfig } from "sst";
+import { StaticSite } from "sst/constructs";
+import { HttpVersion } from "aws-cdk-lib/aws-cloudfront";
+
+export default {
+  config(_input) {
+    return {
+      name: "personal-site",
+      region: "ap-southeast-1",
+    };
+  },
+  stacks(app) {
+    app.stack(function Site({ stack }) {
+      const site = new StaticSite(stack, "site", {
+        buildOutput: "dist",
+        buildCommand: "pnpm build",
+        customDomain: {
+          domainName: "phucn.dev",
+          domainAlias: "www.phucn.dev",
+        },
+        cdk: {
+          distribution: {
+            httpVersion: HttpVersion.HTTP2_AND_3,
+          },
+        },
+      });
+      stack.addOutputs({
+        Url: site.customDomainUrl || site.url,
+      });
+    });
+  },
+} satisfies SSTConfig;
