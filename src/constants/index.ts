2a794b61ed44188b99f63ca8800c074056eccaf9
Phúc Nguyễn <40336551+phucnguyen035@users.noreply.github.com>
2023-10-15 15:22:28 +0700

    feat: add contacts

---
+++ src/constants/index.ts
@@ -0,0 +1,5 @@
+export const CONTACTS = [
+  { platform: 'linkedin', url: 'https://www.linkedin.com/in/phucnguyen035' },
+  { platform: 'github', url: 'https://github.com/phucnguyen035' },
+  { platform: 'email', url: 'mailto:me@phucn.dev' },
+] as const;
