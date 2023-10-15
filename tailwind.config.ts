7cacd444dd40275387cc37f2637cd06bef1898b4
Phúc Nguyễn <40336551+phucnguyen035@users.noreply.github.com>
2023-10-15 15:33:06 +0700

    refactor: change tailwind dark mode to class

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,7 @@ import type { Config } from 'tailwindcss';
 import defaultTheme from 'tailwindcss/defaultTheme';
 
 export default {
-  darkMode: 'media',
+  darkMode: 'class',
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
