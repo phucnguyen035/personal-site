12b4e14fddb2bd8f3a24f450559fcd260cc5d358
Phúc Nguyễn <40336551+phucnguyen035@users.noreply.github.com>
2023-10-15 14:32:06 +0700

    chore(sst): add staging env

--- sst.config.ts
+++ sst.config.ts
@@ -1,23 +1,23 @@
-import type { SSTConfig } from "sst";
-import { StaticSite } from "sst/constructs";
-import { HttpVersion } from "aws-cdk-lib/aws-cloudfront";
+import type { SSTConfig } from 'sst';
+import { StaticSite } from 'sst/constructs';
+import { HttpVersion } from 'aws-cdk-lib/aws-cloudfront';
 
 export default {
   config(_input) {
     return {
-      name: "personal-site",
-      region: "ap-southeast-1",
+      name: 'personal-site',
+      region: 'ap-southeast-1',
     };
   },
   stacks(app) {
     app.stack(function Site({ stack }) {
-      const site = new StaticSite(stack, "site", {
-        buildOutput: "dist",
-        buildCommand: "pnpm build",
-        customDomain: {
-          domainName: "phucn.dev",
-          domainAlias: "www.phucn.dev",
-        },
+      const site = new StaticSite(stack, 'site', {
+        buildOutput: 'dist',
+        buildCommand: 'pnpm build',
+        customDomain:
+          stack.stage === 'prod'
+            ? { domainName: 'phucn.dev', domainAlias: 'www.phucn.dev' }
+            : `${stack.stage}.phucn.dev`,
         cdk: {
           distribution: {
             httpVersion: HttpVersion.HTTP2_AND_3,
