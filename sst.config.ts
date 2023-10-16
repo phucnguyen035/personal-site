6b69ab4e4d63ee1950bc3030a85a96a0dbf13022
Phúc Nguyễn <40336551+phucnguyen035@users.noreply.github.com>
2023-10-16 16:14:21 +0700

    fix: file options, disable viewer protocol policy

--- sst.config.ts
+++ sst.config.ts
@@ -20,29 +20,37 @@ export default {
           domainAlias: stack.stage === 'prod' ? 'www.phucn.dev' : '',
           hostedZone: 'phucn.dev',
         },
-        // fileOptions: [
-        //   { exclude: '*', include: '*.woff2', cacheControl: 'public,max-age=31536000,immutable' },
-        // ],
+        fileOptions: [
+          {
+            exclude: '*',
+            include: '*.html',
+            cacheControl: 'max-age=0,no-cache,no-store,must-revalidate',
+          },
+          {
+            exclude: '*',
+            include: ['*.js', '*.css', '*.woff2'],
+            cacheControl: 'max-age=31536000,public,immutable',
+          },
+        ],
         cdk: {
           distribution: {
             comment: 'Personal site CDK distribution',
             minimumProtocolVersion: cf.SecurityPolicyProtocol.TLS_V1_2_2021,
             httpVersion: cf.HttpVersion.HTTP2_AND_3,
             defaultBehavior: {
-              viewerProtocolPolicy: cf.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
               responseHeadersPolicy: new cf.ResponseHeadersPolicy(stack, 'ResponseHeaderPolicy', {
                 comment: 'Security headers response header policy',
                 securityHeadersBehavior: {
-                  contentSecurityPolicy: {
-                    override: true,
-                    contentSecurityPolicy: "default-src 'unsafe-inline' https:;",
-                  },
-                  // strictTransportSecurity: {
+                  // contentSecurityPolicy: {
                   //   override: true,
-                  //   accessControlMaxAge: cdk.Duration.days(365),
-                  //   includeSubdomains: true,
-                  //   preload: true,
+                  //   contentSecurityPolicy: "default-src 'unsafe-inline' https:;",
                   // },
+                  strictTransportSecurity: {
+                    override: true,
+                    accessControlMaxAge: cdk.Duration.days(365),
+                    includeSubdomains: true,
+                    preload: true,
+                  },
                   xssProtection: {
                     modeBlock: true,
                     override: true,
